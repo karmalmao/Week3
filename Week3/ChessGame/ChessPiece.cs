a6f458758514266e14ca9936019a8c03d0036f10
Jack Norrie <s210193@students.aie.edu.au>
2021-02-15 16:50:35 +1100

    kmao

---
+++ Week3/ChessGame/ChessPiece.cs
@@ -0,0 +1,295 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame
+{
+    class ChessPiece
+    {
+        public enum Type
+        {
+            PAWN,
+            ROOK,
+            BISHOP,
+            KNIGHT,
+            QUEEN,
+            KING
+        }
+
+        protected ChessBoard board;
+
+        EChessSide side;
+        int row;
+        int col;
+        Type type;
+        
+        public ChessPiece(ChessBoard board, EChessSide side, Type type, int row, int col)
+        {
+            this.board = board;
+            this.side = side;
+            this.type = type;
+            this.row = row;
+            this.col = col;
+        }
+
+        public virtual bool IsValidMove(int targetRow, int targetCol)
+        {
+            return false;
+        }
+
+        public int GetRow()
+        {
+            return row;
+        }
+
+        public int GetCol()
+        {
+            return col;
+        }
+
+        public Type GetPieceType()
+        {
+            return type;
+        }
+
+        public EChessSide GetSide()
+        {
+            return side;
+        }
+
+        public void MoveTo(int row, int col)
+        {
+            board.SetBoardPiece(this.row, this.col, null);
+            this.row = row;
+            this.col = col;
+            board.SetBoardPiece(this.row, this.col, this);
+            
+        }
+
+    }
+
+    class ChessPieceKnight : ChessPiece
+    {
+        public ChessPieceKnight(ChessBoard board, EChessSide side, int row, int col) : 
+            base(board, side, Type.KNIGHT, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+
+            if(GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol() + 1 || targetCol == GetCol() - 1 || targetCol == GetCol() - 2 || targetCol == GetCol() + 2)
+                {
+                    
+                    if (GetRow() + 2 == targetRow || GetRow() - 2 == targetRow || GetRow() - 1 == targetRow || GetRow() - 2 == targetRow)
+                        return true;
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol() + 1 || targetCol == GetCol() - 1)
+                {
+
+                    if (GetRow() + 2 == targetRow || GetRow() - 2 == targetRow)
+                        return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+    class ChessPieceRook : ChessPiece
+    {
+        public ChessPieceRook(ChessBoard board, EChessSide side, int row, int col) :
+            base(board, side, Type.ROOK, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+
+            if (GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol())
+                {
+
+                }
+                if(targetRow == GetRow())
+                {
+
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol())
+                {
+
+                    for (int i = 1; i < 8; i++)
+                    {
+                        if (GetCol() + i == targetRow)
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+    class ChessPieceQueen : ChessPiece
+    {
+        public ChessPieceQueen(ChessBoard board, EChessSide side, int row, int col) :
+            base(board, side, Type.QUEEN, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+
+            if (GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() + 1 == targetRow)
+                        return true;
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() - 1 == targetRow)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    class ChessPieceBishop : ChessPiece
+    {
+        public ChessPieceBishop(ChessBoard board, EChessSide side, int row, int col) :
+            base(board, side, Type.BISHOP, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+            if (targetPiece != null && targetPiece.GetSide() != GetSide())
+                return false;
+
+
+            if (GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() + 1 == targetRow && GetCol() + 1 == targetRow)
+                        return true;
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() - 1 == targetRow)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    class ChessPiecePawn : ChessPiece
+    {
+        public ChessPiecePawn(ChessBoard board, EChessSide side, int row, int col) :
+            base(board, side, Type.PAWN, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+
+            if (GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() + 2 == targetRow)
+                        return true;
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() - 2 == targetRow)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+    class ChessPieceKing : ChessPiece
+    {
+        public ChessPieceKing(ChessBoard board, EChessSide side, int row, int col) :
+            base(board, side, Type.KING, row, col)
+        {
+
+        }
+
+        public override bool IsValidMove(int targetRow, int targetCol)
+        {
+
+            var targetPiece = board.GetPiece(targetRow, targetCol);
+            if (targetPiece != null && targetPiece.GetSide() == GetSide())
+                return false;
+
+            if (GetSide() == EChessSide.WHITE)
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() + 1 == targetRow)
+                        return true;
+                }
+            }
+            else
+            {
+                if (targetCol == GetCol())
+                {
+                    if (GetRow() - 1 == targetRow)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ Week3/Week3/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+namespace inheritance
+{
+    class Program
+    {
+        public static Person[] persons = new Person[3];
+        public static void Main(string[] args)
+        {
+            persons[0] = new Grunt();
+            persons[1] = new Brute();
+            persons[2] = new Mage();
+
+            bool able = true;
+            int state = 1;
+            string test = "";
+            string again = "";
+
+            while (able)
+            {
+                if (state == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You are approached by a Mage, Brute, and Grunt");
+                    Sleep(2000);
+                    Console.Clear();
+                    Console.WriteLine("What will you do? ");
+                    state = 2;
+                    while (state == 2)
+                    {
+                        test = Console.ReadLine();
+                        test.ToLower();
+                        able = false;
+                        if (test == "fight")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You engage in combat with the enemies");
+                            Sleep(2000);
+                            Console.Clear();
+                            state = 4;
+                        }
+                        if (test == "run" || test == "flee")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You flee from the scene");
+                            Sleep(3000);
+                            Console.Clear();
+                            Console.WriteLine("Coward");
+                            Sleep(300);
+                            Console.Clear();
+                            state = 3;
+                        }
+                    }
+                    if (state == 4)
+                    {
+                        Console.WriteLine("You are now fighting the enemies");
+                        Sleep(2000);
+                        Console.Clear();
+                        Console.WriteLine("You are not strong enough to take 3 enemies");
+                        Sleep(2000);
+                        Console.Clear();
+                        foreach (var p in persons) { p.Attack(); Console.WriteLine("");  Sleep(2000); }
+                        Console.WriteLine("You died");
+                    }
+                    if (state == 3)
+                    {
+                        Console.WriteLine("The enemies start to chase you.");
+                        Sleep(2000);
+                        Console.Clear();
+                        Console.WriteLine("You cannot escape from the enemies");
+                        Sleep(2000);
+                        Console.Clear();
+                        Console.WriteLine("You are now fighting the enemies");
+                        Sleep(2000);
+                        Console.Clear();
+                        Console.WriteLine("You are not strong enough to take 3 enemies");
+                        Sleep(2000);
+                        Console.Clear();
+                        foreach (var p in persons) { p.Attack(); Console.WriteLine(""); Sleep(2000); }
+                        Console.Clear();
+                        Console.WriteLine("You died");
+                        Sleep(2000);
+                    }
+                    Console.WriteLine("Would you like to try again? ");
+                    again = Console.ReadLine();
+                    again.ToLower();
+                    if (again == "yes") {able = true; state = 1;}
+                else able = false;
+
+
+
+                }
+                
+
+
+            }
+            
+            
+            
+            
+            
+            
+            
+           
+
+
+        }
+        public static void Sleep(int a)
+        {
+            Thread.Sleep(a);
+        }
+    }
+    class Person
+    {
+        public string item = "";
+        public string name = "";
+
+        public void displayItem(Array a)
+        {
+            Console.WriteLine($"{name} is holding {item}.");
+        }
+       public virtual void Attack()
+        {
+            Console.WriteLine($"{name} attacks you with their {item}.");
+        }
+    }
+    class Grunt : Person
+    {
+        public Grunt() : base()
+        {
+            item = "axe";
+            name = "Grunt";
+        }
+    }
+    class Brute : Person
+    {
+       public Brute() : base()
+        {
+            item = "hammer";
+            name = "Brute";
+        }
+    }
+    class Mage : Person
+    {
+        public Mage() : base()
+        {
+            item = "staff";
+            name = "Mage";
+        }
+        public override void Attack()
+        {
+            Console.WriteLine($"{name} casts firebolt at you with their {item}.");
+        }
+    }
+
+}
